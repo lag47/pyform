8c50b7e645ef3e7d4785c88aba546b7983efe861
lag47 <lag4777@gmail.com>
2016-12-18 18:33:17 -0500

    began implementing draw

--- planecanvas.py
+++ planecanvas.py
@@ -1,7 +1,17 @@
 import coplane
 class PlaneCanvas:
 	plane
-	def __init__(plane):
+	origin
+	scale
+	canvas
+	def __init__(self,plane,origin,scale,canvas):
 		self.plane=plane
+		self.origin=origin
+		self.scale=scale
+		self.canvas=canvas
 	def draw(c):
+		vectors=plane.vectors()
+		for x in vectors:
+			y=x#transform to scale
+			canvas.create_line(y[0],y[1])
 		return None
